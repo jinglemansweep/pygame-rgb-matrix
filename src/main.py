f2eeac1fee5f829965a3b529dd0ba423040b543b
Louis King <jinglemansweep@gmail.com>
2022-12-04 14:10:38 +0000

    Dynamic maps

--- src/main.py
+++ src/main.py
@@ -34,6 +34,33 @@ COLOR_GREY_DARK = (15, 15, 15)
 COLOR_WHITE = (255, 255, 255)
 COLOR_BACKGROUND = (0, 0, 0)
 
+# THEME STUFF
+
+TILE_GRASS_EMPTY_ALT = 0
+TILE_GRASS_EMPTY = 1
+TILE_GRASS_BEACH = 2
+TILE_GRASS_BEACH_ALT = 8
+TILE_GRASS_ROCKS = 6
+TILE_GRASS_WEEDS = 7
+
+TILE_DESERT_EMPTY_ALT = 3
+TILE_DESERT_EMPTY = 4
+TILE_DESERT_BEACH = 5
+TILE_DESERT_BEACH_ALT = 11
+TILE_DESERT_ROCKS = 9
+TILE_DESERT_WEEDS = 10
+
+TILE_WAVES = 48
+TILE_WAVES_ALT = 54
+
+TILE_WATER_EMPTY = 49
+TILE_WATER_LILY = 50
+TILE_WATER_REEDS = 56
+
+MAP_TILE_SIZE = 8
+MAP_WIDTH = 32  # multiplied by tile size to get pixels
+MAP_HEIGHT = 32  # multiplied by tile size to get pixels
+
 SPRITE_TILES_FILE = f"{os.path.dirname(__file__)}/sprites/tiles.png"
 SPRITE_ANIMALS_FILE = f"{os.path.dirname(__file__)}/sprites/animals.png"
 
@@ -41,27 +68,70 @@ tileset_bg = Tileset(SPRITE_TILES_FILE, (8, 8), 0, 0, 0.6)
 tilemap_bg = Tilemap(tileset_bg, (8, 8))
 
 
-def generate_bg_row():
-    row = [0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(0, 2):
-        row[random.randint(0, 7)] = 1
-    if random.randint(0, 3) == 0:
-        row[random.randint(0, 7)] = 6
+def generate_row_land(width, alt_tiles=0, rock_tiles=0, weed_tiles=0, desert=False):
+    start_tile = TILE_DESERT_EMPTY_ALT if desert else TILE_GRASS_EMPTY_ALT
+    row = [start_tile + 1 for i in range(width)]
+    for i in range(alt_tiles):
+        row[random.randint(0, width - 1)] = start_tile
+    for i in range(rock_tiles):
+        row[random.randint(0, width - 1)] = start_tile + 6
+    for i in range(weed_tiles):
+        row[random.randint(0, width - 1)] = start_tile + 7
+    print(row)
     return row
 
 
-tilemap_bg.set_map(
-    [
-        [1, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1],
-        [2, 8, 2, 8, 2, 2, 2, 2],
-        [48, 54, 48, 54, 48, 54, 48, 54],
-        [49, 49, 49, 49, 49, 49, 49, 49],
-        [49, 50, 49, 49, 49, 49, 49, 49],
-        [49, 49, 49, 49, 56, 49, 49, 49],
-        [49, 49, 49, 49, 49, 49, 49, 49],
-    ]
-)
+def generate_row_beach(width, alt_tiles=0, desert=False):
+    start_tile = TILE_DESERT_EMPTY_ALT if desert else TILE_GRASS_EMPTY_ALT
+    row = [start_tile + 2 for i in range(width)]
+    for i in range(alt_tiles):
+        row[random.randint(0, width - 1)] = start_tile + 8
+    print(row)
+    return row
+
+
+def generate_row_waves(width, alt_tiles=0):
+    row = [TILE_WAVES for i in range(width)]
+    for i in range(alt_tiles):
+        row[random.randint(0, width - 1)] = TILE_WAVES_ALT
+    print(row)
+    return row
+
+
+def generate_row_water(width, lily_tiles=0, reed_tiles=0):
+    row = [TILE_WATER_EMPTY for i in range(width)]
+    for i in range(lily_tiles):
+        row[random.randint(0, width - 1)] = TILE_WATER_LILY
+    for i in range(reed_tiles):
+        row[random.randint(0, width - 1)] = TILE_WATER_REEDS
+    print(row)
+    return row
+
+
+row_beach = random.randint(3, 6)
+
+game_map = []
+
+for row in range(MAP_HEIGHT):
+    if row < row_beach:
+        game_map.append(
+            generate_row_land(
+                MAP_WIDTH,
+                random.randint(0, 2),
+                random.randint(0, 2),
+                random.randint(0, 2),
+            )
+        )
+    if row == row_beach:
+        game_map.append(generate_row_beach(MAP_WIDTH, random.randint(0, 4)))
+    if row == row_beach + 1:
+        game_map.append(generate_row_waves(MAP_WIDTH, random.randint(0, 2)))
+    if row > row_beach + 1:
+        game_map.append(
+            generate_row_water(MAP_WIDTH, random.randint(0, 2), random.randint(0, 2))
+        )
+
+tilemap_bg.set_map(game_map)
 tilemap_bg.render()
 
 tileset_animals = Tileset(SPRITE_ANIMALS_FILE, (8, 8), 0, 0, 0.9)
