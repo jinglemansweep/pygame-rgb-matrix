1a7eb897a2bbb71d21f41dd4e1bc7269575bd1dd
Louis King <jinglemansweep@gmail.com>
2023-02-05 22:07:21 +0000

    Tweak

--- src/app/sprites/utils/images.py
+++ src/app/sprites/utils/images.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("sprites.utils.images")
 def load_and_resize_image(filename, size):
     with Image.open(filename) as im:
         im.thumbnail(size, Image.Resampling.LANCZOS)
-        image = pygame.image.fromstring(im.tobytes(), im.size, im.mode).convert()
+        image = pygame.image.fromstring(im.tobytes(), im.size, im.mode).convert_alpha()
     return image
 
 
